c9f6c142ec73e67ccc6404a23b2d3c374063036a
Knox Hutchinson <47797909+DataKnox@users.noreply.github.com>
2019-07-15 10:31:39 -0500

    Added lines to RESTCONF.ps1
    Added conditional lines

--- Powershell/Networking/RESTCONF.ps1
+++ Powershell/Networking/RESTCONF.ps1
@@ -1,5 +1,5 @@
 $uri = 'https://ios-xe-mgmt.cisco.com:9443/restconf/data/Cisco-IOS-XE-interfaces-oper:interfaces/interface=GigabitEthernet1'
-$creds = Get-Credential
+$creds = Get-Credential #root D_Vay!_10&
 $headers = @{'Accept' = 'application/yang-data+json' }
 
 $response = Invoke-RestMethod -Uri $uri `
@@ -9,6 +9,11 @@ $response = Invoke-RestMethod -Uri $uri `
     -ContentType 'application/yang-data+json' `
     -Headers $headers `
     -SkipCertificateCheck
+
 $response | ConvertTo-Json | Write-Output
 
 $response.'Cisco-IOS-XE-interfaces-oper:interface'.name
+
+if ($response.'Cisco-IOS-XE-interfaces-oper:interface'.'admin-status' = 'if-state-up') {
+    Write-Host ($response.'Cisco-IOS-XE-interfaces-oper:interface'.name)'is up'
+}
